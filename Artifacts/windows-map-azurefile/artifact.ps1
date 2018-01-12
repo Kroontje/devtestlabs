4bfef518ba7d07996fa3806c9699d8254c3175f2
Kroontje <stefan@stfnkrn.nl>
2018-01-12 14:04:31 +0100

    Create artifact.ps1

---
+++ Artifacts/windows-map-azurefile/artifact.ps1
@@ -0,0 +1,24 @@
+[CmdletBinding()]       
+param
+(
+    [Parameter(Mandatory = $true)]
+    [string] $shareStorageAccount,
+
+    [Parameter(Mandatory = $true)]
+    [string] $shareSecretKey
+)
+
+##############################
+# Main function
+
+if ($PSVersionTable.PSVersion.Major -lt 3)
+{
+    Write-Error "The current version of PowerShell is $($PSVersionTable.PSVersion.Major). Prior to running this artifact, ensure you have PowerShell 3 or higher installed."
+}
+
+else
+{
+    $acctKey = ConvertTo-SecureString -String $shareSecretKey -AsPlainText -Force
+    $credential = New-Object System.Management.Automation.PSCredential -ArgumentList "Azure\$shareStorageAccount", $acctKey
+    New-PSDrive -Name X -PSProvider FileSystem -Root "\\$shareStorageAccount.file.core.windows.net\demoshare" -Credential $credential -Persist
+}
